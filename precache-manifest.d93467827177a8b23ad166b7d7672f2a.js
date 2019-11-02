1c27d8d57affac543cdd91a49b935b597dd85163
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-02 21:16:23 +0300

    Updates

---
+++ precache-manifest.d93467827177a8b23ad166b7d7672f2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae579aeb73f60bd2b7499e2f4b4caf0",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "722c03924f9c7ad7cd2e",
+    "url": "/react_dynamic-list-of-posts/static/css/main.d2d22e21.chunk.css"
+  },
+  {
+    "revision": "a211c0966253e7239b65",
+    "url": "/react_dynamic-list-of-posts/static/js/2.740e0f14.chunk.js"
+  },
+  {
+    "revision": "722c03924f9c7ad7cd2e",
+    "url": "/react_dynamic-list-of-posts/static/js/main.4e46eeb9.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
